d1c08f208c20f09548e9b8ea3f3816a9f54e7646
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-18 10:44:03 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,5 @@
 class Application
-
+  @@cart = []
   @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
@@ -13,10 +13,20 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 0 
+        @@cart.each do |item|
+         resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end 
+    elsif req.path.match(/add/)
+      add_term = req.params["item"]
+      resp.write add_search(add_term)
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
@@ -27,4 +37,15 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def add_search(add_term)
+    if @@items.include?(add_term)
+      @@cart << add_term 
+      return "added #{add_term}"
+    else 
+      return "We don't have that item"
+    end 
+  end 
+
+
 end
